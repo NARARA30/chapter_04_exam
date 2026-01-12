814f63b7c129b0b7de18740474572e8d0876050f
NARARA30 <nara.makne0@gmail.com>
2026-01-12 17:59:00 +0900

    input 추가

---
+++ function_module.py
@@ -0,0 +1,22 @@
+#len() 자료의 길이를 알려주는 함수
+my_list = ['a','b','c']
+
+print(len(my_list))
+
+#int() 정수형으로 변환하는 함수
+my_date = '1234'
+
+print(type(int(my_date)))
+
+print(int(my_date))
+
+#str() 문자열로 변환하는 함수
+my_number = 5678
+
+print(type(str(my_number)))
+
+print(str(my_number))
+
+
+#input() 자료를 입력하는 함수
+#print() 자료를 출력하는 함수
