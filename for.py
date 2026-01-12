2c29be6420a04618b155e6e6e2e03202ffdf0690
NARARA30 <nara.makne0@gmail.com>
2026-01-12 18:12:34 +0900

    주석 추가

--- for.py
+++ for.py
@@ -1,3 +1,5 @@
+#for 문 원소로 반복하는 방법
+# 시퀀스의 원소를 하나씩 변수에 넣어가면서 명령 실행
 sum = 0
 
 for num in [1,2,3,4,5]:
@@ -10,6 +12,7 @@ for i in [1,3,5]:
 
 print(sum)
 
+# 명령이 len(시퀀스)번 만큼 실행
 length = 0
 
 for i in [1,3,5]:
@@ -17,6 +20,8 @@ for i in [1,3,5]:
 
 print(length)
 
+# 연속되는 숫자를 만ㄷ늘어 주는 시퀀스 자료형
+# 시작하는 숫자(a)와 끝내는 숫자(b)를 지정하면 a~ (b-1)까지의 숫자 생성
 print(range(1,9))
 
 print(list(range(1,9)))
@@ -32,7 +37,8 @@ for i in range(2,5):
 
 print(num_list)
 
-
+# 횟수로 반복하는 방법
+# a번 만큼 명령을 수행
 count = 0
 
 for i in range(10):
@@ -40,7 +46,7 @@ for i in range(10):
 
 print(count)
 
-
+# range와 len, 인덱싱을 이용할 수 있음
 str_list = ['a','b', 'c','d']
 
 for idx in range(len(str_list)):
@@ -49,6 +55,8 @@ for idx in range(len(str_list)):
 for idx in range(len(str_list)):
     print(str_list[idx])
 
+# 조건으로 반복하는 방법
+# 조건이 True이면 명령을 수행
 number = 5
 
 while number > 0 :
@@ -57,6 +65,7 @@ while number > 0 :
 
 print('good day!!')
 
+# 1부터 4까지 더하기
 i=1
 sum=0
 
@@ -66,7 +75,7 @@ while i < 5:
 
 print(sum)
 
-
+# 무한정 코드가 실행되어 무한루프 상태가 됨 (실행시키면 안됨)
 #i=1
 
 #while i >0:
@@ -75,7 +84,9 @@ print(sum)
 
 #print('종료')
 
-
+# break 문
+# if 문으로 조건을 걸어준 다음, break 문 실행.
+# 반복문을 탈출하는 역할
 number = 1
 
 while True : 
@@ -88,7 +99,8 @@ while True :
 
 print(number)
 
-
+# continue 문
+# if 문으로 조건을 걸어준 다음, continue 실행. 반복문을 건너뛰는 역할!
 for number in range(1,11) :
 
     if number % 2 != 0 :
--- if01.py
+++ if01.py
@@ -1,17 +1,23 @@
+#참 또는 거짓을 나따내는 자료형
 print(True)
 print(False)
 
+#숫자나 문자의 값을 비교하는 연산자
 print(3 < 5)
 print(7 == 5)
 print(2 >= 10)
 print(5! =10)
 
+# 각 논리가 모두 True여야 True
 print(3==3 and 4<=5 and 6>2)
 
+# 논리들 중 하나라도 True가 존재하면 True
 print(3==4 or 4<=5 or 6<2)
 
+#논리값을 뒤집음(True - False)
 print(not 3==4)
 
+#if 문
 i=1
 
 if i == 1:
