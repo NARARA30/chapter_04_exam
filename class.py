427652388b2a5dd2b5a1f4894df7b5f4a617cac4
NARARA30 <nara.makne0@gmail.com>
2026-01-13 17:33:15 +0900

    원하지 않는 값 배제하기

--- class.py
+++ class.py
@@ -70,4 +70,78 @@ print(my_post.num_likes())
 
 
 
-#
+#원하지 않은 값 배제하기
+class Post :
+    def __init__(self):
+        self,liked_user = []
+     def num_likes(self):
+        return len(self.liked_users)
+    
+my_post = Post('elice', 1457)
+my_post.like(['Hello', 'World']')
+             
+
+
+#원하지 않은 값 배제하기(1)
+# year_of_birth의 데이터 타입을 검사
+## 숫자(int)가 아니라면 return 명령어 실행 -> 초기화 중단
+class User:
+    def __init__ (self, year_of_birth):
+        if type(year_of_birth) is not int :
+            return
+        self.year_of_birth = year_of_birth
+
+user1 = User(1994)
+user2 = User('1994')
+
+print(user1.year_of_birth)
+print(user2.year_of_birth)
+
+
+
+#원하지 않는 값 배제하기(2)
+class User :
+    def __init__(self, name):
+        self.name = name
+
+class Post:
+    def __init__(self, author, content):
+        if not isinstance(author, User):
+            return
+        if type(content) is not str:
+            return
+
+
+author = User('elice')
+my_post = Post.author('I Love Coding!')
+
+print(my_post.author.name)
+print(my_post.content)
+
+
+your_post = Post('hellobit','Hello World!')
+
+print(your_post.author.name)
+print(your_post.content)
+
+
+#[ref]isinstance??
+## 객체의 자료형(타입)을 검사할 때 사용하는 python 내장 함수
+### 첫번째 인자(객체) : 검사할 객체
+### 두번째 인자(클래스 or 타입) : 확인할 클래스 또는 타입
+### 반환값 : True or False
+# isinstance(객체, 클래스 or 타입)
+print(isinstance(5,int))
+print(isinstance('hello', str))
+print(isinstance(3.14, int))
+
+
+class User :
+    def __init__ (self, year_of_birth) :
+        if year_of_birth > 2005 :
+            raise Exception('Too young')
+
+user1 = User(2000)
+user2 = User(2006)
+
+
